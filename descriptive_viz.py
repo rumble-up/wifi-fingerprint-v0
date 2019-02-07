2130798da5425c621ed8aa168219aa87f3ce74b3
L Stupin <laura.stupin@gmail.com>
2019-02-07 16:32:04 +0100

    WAP average signal and number of observations plot

--- descriptive_viz.py
+++ descriptive_viz.py
@@ -25,17 +25,18 @@ import numpy as np
 df = pd.read_csv('data/trainingData.csv')
 
 # Keep columns that correspond to WAPs.
-waps = [col for col in df if col.startswith('WAP')]
-df = df[waps]
+wap_names = [col for col in df if col.startswith('WAP')]
+df = df[wap_names]
 # Put all signals in the same column
-df_m = pd.melt(df, value_vars = waps)
-len(df_m)
+df_m = pd.melt(df, value_vars = wap_names)
+
 
 # Remove all 100 values
 df_m = df_m[df_m.value != 100]
 
 print(min(df_m.value))
 print(max(df_m.value))
+
 # Plot total histogram
 data = [go.Histogram(x=df_m['value'])]
 plot(data)
@@ -50,7 +51,7 @@ plot(data)
 df_na = df.replace(100, np.NaN)
 
 # Plot average signal strength/WAP histogram
-avg = df_na.mean(axis = 0, skipna = True).sort_values()
+avg = df_na.mean(axis = 0, skipna = True).sort_values(ascending = False)
 data = [go.Histogram(x=avg)]
 plot(data, filename = 'plots/avg_sig_histogram.html')
 
@@ -60,3 +61,31 @@ na_count = df_na.isna().sum(axis =1)
 non_na = 520 - na_count
 data = [go.Histogram(x=non_na)]
 plot(data)
+
+
+# Scatter plots
+# Average signal by WAP
+avg = avg.sort_index()
+# Observations by WAP
+obs = df_na.count()
+
+# Dataframe of characteristics by WAP
+df_wap = pd.DataFrame(dict(avg = avg, obs = obs))
+
+trace = go.Scatter(
+        x = df_wap.index,
+        y = df_wap.avg, 
+        text = df_wap.obs,
+        hoverinfo = 'text',
+        mode = 'markers',
+        marker = dict(
+                color = np.log(df_wap.obs),
+                colorscale = 'Viridis',
+                showscale = True
+        )
+)
+
+data = [trace]
+plot(data)
+
+
