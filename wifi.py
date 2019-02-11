65a57cf71247d6310ce2c6a28cb445e056b93d4d
Deniz <deniz.minican@gmail.com>
2019-02-11 10:47:37 +0100

    add deniz script

---
+++ wifi.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#%%
+## Importing Packages
+import numpy as np  # for calculations
+import pandas as pd  # for dataframes
+import sklearn  # for caret
+# import statsmodel.api as sm  # DOESN'T WORK
+import matplotlib  # for ggplot
+import matplotlib.pyplot as plt
+plt.close('all')
+from mpl_toolkits.mplot3d import Axes3D 
+import seaborn  # for ggplot
+import plotly.plotly as py
+import plotly.graph_objs as go
+import os, sys
+import csv
+from datetime import datetime
+
+#%%
+## Get and change working directory
+os.getcwd()
+os.chdir('/Users/denizminican/Dropbox/03-Data_and_Coding/Ubiqum/Wifi')
+
+#%%
+## Create the df
+train = pd.read_csv('trainingData.csv')
+
+## Change Time to DateTime
+# =============================================================================
+# train['TIMESTAMP'] = pd.to_datetime(train['TIMESTAMP'])
+# =============================================================================
+
+
+#%%
+## Initial plot
+list(train)
+mainplot = train.plot.scatter(x= "LATITUDE", y= "LONGITUDE")
+
+## Finding the entries of users seperately
+users = np.unique(train["USERID"])
+plots = {}
+for user in users: 
+    subset = train.loc[train["USERID"] == user,:]
+    plots[user] = subset.plot.scatter(x= "LATITUDE", y= "LONGITUDE")
+
+## Plot with color per user
+users = np.unique(train["USERID"])
+train.plot.scatter(x= "LATITUDE", y= "LONGITUDE", c= "USERID")  # how to put colors instead of grayscale
+
+#%%
+## PLot 3D with floors
+tdplot = plt.figure().gca(projection='3d')
+tdplot.scatter(xs=train["LATITUDE"], ys=train["LONGITUDE"], zs=train["FLOOR"], c=train["FLOOR"])
+tdplot.set_zlabel('Floor')
+plt.show()
+plt.figure()
+
+#%%
+## Plotly plots
+
+
+#%%
+## PhoneID dBm boxplots
+train_phone = train.drop(['LATITUDE', 'LONGITUDE', 'USERID', 'FLOOR', 'BUILDINGID', 'SPACEID',
+                          'RELATIVEPOSITION', 'TIMESTAMP'], axis = 1)
+
+melted_phone = pd.melt(train_phone, id_vars=['PHONEID'], var_name='WAP')
+melted_phone = melted_phone[melted_phone['value'] != 100]
+melted_phone.boxplot(by = "PHONEID", column = "value")
+
+#%%
+## WAP distribution per building and floor
+# Melt for long format
+melted_table = pd.melt(train, id_vars=['PHONEID', 'LATITUDE', 'LONGITUDE', 'USERID', 
+                                       'FLOOR', 'BUILDINGID', 'SPACEID', 'RELATIVEPOSITION', 
+                                       'TIMESTAMP'], var_name='WAP')
+# Remove unused WAPs
+melted_table = melted_table[melted_table['value'] != 100]
+
+# Check missing info
+melted_table['FLOOR'].unique()
+buildings = melted_table['BUILDINGID'].unique()
+
+# Melted buildings
+# =============================================================================
+# buildings = [0,1,2]
+# building_list = []
+# for i in buildings:
+#     building_list[i] = melted_table[melted_table['BUILDINGID'] == i]
+# =============================================================================
+    
+building0 = melted_table[melted_table['BUILDINGID'] == 0]
+building1 = melted_table[melted_table['BUILDINGID'] == 1]
+building2 = melted_table[melted_table['BUILDINGID'] == 2] 
+wap0 = building0['WAP'].unique().tolist()
+wap1 = building1['WAP'].unique().tolist()
+wap2 = building2['WAP'].unique().tolist()
+
+wap01 = list(set(wap0) & set(wap1))
+wap12 = list(set(wap1) & set(wap2))
+wap02 = list(set(wap0) & set(wap2))
+
+
