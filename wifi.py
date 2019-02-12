c0a331eaf17c441b2a4409fcbaec0727b0e78a25
Deniz <deniz.minican@gmail.com>
2019-02-12 11:06:58 +0100

    remove .DS_Store, store list of common APs for b1 and b2

--- wifi.py
+++ wifi.py
@@ -1,30 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 #%%
 ## Importing Packages
 import numpy as np  # for calculations
 import pandas as pd  # for dataframes
 import sklearn  # for caret
 # import statsmodel.api as sm  # DOESN'T WORK
-import matplotlib  # for ggplot
 import matplotlib.pyplot as plt
 plt.close('all')
 from mpl_toolkits.mplot3d import Axes3D 
-import seaborn  # for ggplot
 import plotly.plotly as py
 import plotly.graph_objs as go
-
-from os import chdir, getcwd
-wd=getcwd()
-chdir(wd)
-
+import seaborn
+import os, sys
 import csv
-from datetime import datetime
 
+import pickle
 #%%
 ## Get and change working directory
 os.getcwd()
@@ -86,7 +75,7 @@ melted_table = pd.melt(train, id_vars=['PHONEID', 'LATITUDE', 'LONGITUDE', 'USER
 # Remove unused WAPs
 melted_table = melted_table[melted_table['value'] != 100]
 
-# Check missing info
+# Check buildings and floors
 melted_table['FLOOR'].unique()
 buildings = melted_table['BUILDINGID'].unique()
 
@@ -105,8 +94,60 @@ wap0 = building0['WAP'].unique().tolist()
 wap1 = building1['WAP'].unique().tolist()
 wap2 = building2['WAP'].unique().tolist()
 
-wap01 = list(set(wap0) & set(wap1))
+# Same APs that are showing up in different buildings
+wap01 = list(set(wap0) & set(wap1))  
 wap12 = list(set(wap1) & set(wap2))
 wap02 = list(set(wap0) & set(wap2))
 
+#%%
+# Saving objects to file
 
+with open('data/file_wap01.pkl', 'wb') as f:
+    pickle.dump(wap01, f)
+
+#%%
+# Frequency of the WAPs
+wapcount = melted_table['WAP'].value_counts()
+wapcount.plot.hist()
+
+py.iplot(go.Histogram(), filename='wap frequency histogram')  # ValueError: The first argument to the plotly.graph_objs.Histogram constructor must be a dict or an instance of plotly.graph_objs.Histogram 
+
+
+#%%
+## Check unique train/validation points
+# Load validation set
+val = pd.read_csv('data/validationData.csv')
+# Melt for long format
+melted_val = pd.melt(val, id_vars=['PHONEID', 'LATITUDE', 'LONGITUDE', 
+                                       'FLOOR', 'BUILDINGID', 
+                                       'TIMESTAMP'], var_name='WAP')
+# Remove unused WAPs
+melted_val = melted_val[melted_val['value'] != 100]
+
+tra_wap = melted_table['WAP'].unique().tolist()  # 465, correct
+val_wap = melted_val['WAP'].unique().tolist()  # Why do i have 370 not 367??
+
+# Finding the common WAPs
+common_waps = list(set(tra_wap) & set(val_wap))  # 312, correct
+
+# Finding the common location points
+tra_loc = melted_table.drop_duplicates(subset= ['LATITUDE', 'LONGITUDE'])  # ask if correct
+tra_loc = tra_loc.drop(['SPACEID', 'RELATIVEPOSITION', 'USERID'], axis = 1)
+val_loc = melted_val.drop_duplicates(subset= ['LATITUDE', 'LONGITUDE'])
+
+#%%
+# Joining them for plotting in same plot
+tra_loc['dataset'] = 'training'
+val_loc['dataset'] = 'validation'
+all_loc = pd.concat([tra_loc, val_loc])
+
+tra_loc.plot.scatter(x= "LATITUDE", y= "LONGITUDE")
+val_loc.plot.scatter(x= "LATITUDE", y= "LONGITUDE")
+
+#%%
+# Training and validation unique points plot
+fg = seaborn.FacetGrid(data=all_loc, hue='dataset')
+fg.map(plt.scatter, 'LATITUDE', 'LONGITUDE').add_legend()
+
+#%%
+# 
