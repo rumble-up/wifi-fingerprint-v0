d8988af4cf0842a4706c899da8b9bd3524f55619
L Stupin <laura.stupin@gmail.com>
2019-02-19 20:33:16 +0100

    Drop null WAPs not in final test set.

--- basic_pred.py
+++ basic_pred.py
@@ -16,7 +16,7 @@ rand = 42          # Set the random seed number
 # %% Model and Data Assumptions -------------------------------------------------
 
 x100 = -110                 # Replace 100s with this value
-drop_null_waps = True       # Drop WAPs not included in both train and validation
+drop_null_waps = True       # Drop WAPs not included in test set
 drop_na_rows = True         # If no WAPs recorded, drop row
 drop_duplicate_rows = False # 
 
@@ -52,16 +52,26 @@ df_val['dataset'] = 'validation'
 df_test = pd.read_csv('data/testData.csv')
 df_test['dataset'] = 'test'
 
+
+# %% Find the null columns ----------------------------------------------------
+dfs = [df_train, df_val, df_test] 
+names = ['train', 'val', 'test']
+
+nulls = dict()
+i = 0
+
+for df1 in dfs:
+    na_cols = df1.replace(100, np.nan).isna().sum()
+    null = na_cols[na_cols == len(df1)].index.tolist()
+    nulls[names[i]] = null
+    i = i+1
+
+
 # Combine datasets so identical pre-processing will happen to all
 df = pd.concat([df_train, df_val, df_test]).reset_index()
 
-# Load previous lists
-with open('data/wap_groups.pkl', 'rb') as f:
-    wap_groups = pickle.load(f)
-    
-null_waps = wap_groups['null_train'] + wap_groups['null_test']
-
 # Drop null WAPs
+null_waps = nulls['test']
 if drop_null_waps: df = df.drop(null_waps, axis=1)
 
 # Collect valid WAP names
