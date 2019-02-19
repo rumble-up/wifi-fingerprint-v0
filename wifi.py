a314fd3a3e8462808d4db1e6ed7451274090793c
Deniz <deniz.minican@gmail.com>
2019-02-19 20:48:43 +0100

    preprocessing to get the 312 common APs

--- wifi.py
+++ wifi.py
@@ -1,20 +1,30 @@
 #%%
+# Computational power
+
+#%%
 ## Importing Packages
 import numpy as np  # for calculations
 import pandas as pd  # for dataframes
-import sklearn  # for caret
+import sklearn
 from sklearn.svm import LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
-# import statsmodel.api as sm  # DOESN'T WORK
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
 import matplotlib.pyplot as plt
 plt.close('all')
 from mpl_toolkits.mplot3d import Axes3D 
 import plotly.plotly as py
 import plotly.graph_objs as go
 import seaborn
-import os, sys
-import csv
+
+import os
+import csv 
+
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
 
 import pickle
 #%%
@@ -25,11 +35,47 @@ os.chdir('/Users/denizminican/Dropbox/03-Data_and_Coding/Ubiqum/Repositories/wif
 #%%
 ## Create the df
 train = pd.read_csv('data/trainingData.csv')
+# Load validation set
+val = pd.read_csv('data/validationData.csv')
 
-## Change Time to DateTime
-# =============================================================================
-# train['TIMESTAMP'] = pd.to_datetime(train['TIMESTAMP'])
-# =============================================================================
+#%%
+### PREPROCESSING
+
+## Changing 100s to something else
+train = train.replace(100, -110)
+val = val.replace(100, -110)
+
+# Prepare train and val waps objects
+train_waps = train.iloc[:, :520]
+val_waps = val.iloc[:, :520]
+val_build = val.iloc[:, 523]
+
+# Reorder columns
+train_dep = train.iloc[:, 520:]
+cols = train_dep.columns.tolist()
+cols = [cols[3]] + [cols [2]] + cols [0:2] + cols[4:]
+train_dep = train_dep[cols]
+train = pd.concat([train_waps, train_dep], axis=1)
+
+## Removing zero variance columns
+train2 = train.loc[:, train.apply(pd.Series.nunique) != 1]
+val2 = val.loc[:, val.apply(pd.Series.nunique) != 1]
+
+# Sampling for train
+sample_train = train2.groupby(['BUILDINGID','FLOOR']).apply(lambda x: x.sample(n=900)) #.reset_index(drop=True)
+# Sampling for test
+# sample_val = val2.groupby(['BUILDINGID','FLOOR']).apply(lambda x: x.sample(n=20))  # too few samples
+
+# Finding common APs and getting them to a list
+tra_cols = list(train.loc[:, train.apply(pd.Series.nunique) == 1].columns.values)
+val_cols = list(val2.columns.values)
+cols312 = list(item for item in val_cols if item not in tra_cols)
+
+# Removing extra APs from the sample_train to have 312 APs
+sample_train = sample_train[cols312] 
+
+# Removing values stronger than -30
+
 
 #%%
 ## Initial plot
@@ -44,7 +90,6 @@ for user in users:
     plots[user] = subset.plot.scatter(x= "LATITUDE", y= "LONGITUDE")
 
 ## Plot with color per user
-users = np.unique(train["USERID"])
 train.plot.scatter(x= "LATITUDE", y= "LONGITUDE", c= "USERID")  # how to put colors instead of grayscale
 
 #%%
@@ -114,13 +159,8 @@ wap02 = list(set(wap0) & set(wap2))
 wapcount = melted_table['WAP'].value_counts()
 wapcount.plot.hist()
 
-py.iplot(go.Histogram(), filename='wap frequency histogram')  # ValueError: The first argument to the 
+py.iplot(go.Histogram())  # ValueError: The first argument to the 
 # plotly.graph_objs.Histogram constructor must be a dict or an instance of plotly.graph_objs.Histogram 
-
-#%%
-## Check unique train/validation points
-# Load validation set
-val = pd.read_csv('data/validationData.csv')
 
 #%%
 # Melt for long format
@@ -155,13 +195,37 @@ val_loc.plot.scatter(x= "LATITUDE", y= "LONGITUDE")
 fg = seaborn.FacetGrid(data=all_loc, hue='dataset')
 fg.map(plt.scatter, 'LATITUDE', 'LONGITUDE').add_legend()
 
-#%%
-## Ranking APs per row
-# Changing 100s to something else
-train = train.replace(100, -110)
-# Finding the top APs for one row and changing others to NAs
-shookones = train.iloc[:,:520].stack().groupby(level=0).nlargest(10).unstack().reset_index(level=1, drop=True).reindex(columns=train.iloc[:,:520].columns)
 
+#%%
+### Taking top k signal WAPs from each row 
+
+## Some loop to choose the top APs
+# K i want for the job
+k = 10  
+
+# Preparing the dfs to fill
+topdf_values = pd.DataFrame(columns=range(k),index=range(19937))
+
+
+# Loop for taking the signal strengths
+for i in range(len(train.index)):
+    topdf_values.iloc[i, :] = train.iloc[i, :520].sort_values(axis = 0, ascending = False)[:k]
+
+# Loop for taking the WAP numbers
+topdf_ranks = train.iloc[:, :520].apply(lambda s: s.nlargest(10).index.tolist(), axis=1)
+topdf_ranks_df = topdf_ranks.to_frame()
+
+deneme = pd.DataFrame(train.iloc[0, :520].sort_values(axis = 0, ascending = False)[:k])
+
+# Cbind row and row2
+
+
+#%%
+# Convert building and floor to categoric ()
+# =============================================================================
+# train['BUILDINGID'] = train['BUILDINGID'].astype('category')
+# train['FLOOR'] = train['FLOOR'].astype('category')
+# =============================================================================
 
 #%%
 #### Modeling
@@ -169,51 +233,51 @@ shookones = train.iloc[:,:520].stack().groupby(level=0).nlargest(10).unstack().r
 ### 1- Basic Model from all data
 
 # Remove the target variables from the training set
-y1 = train.pop('BUILDINGID').values
-y2 = train.pop('FLOOR').values
-y3 = train.pop('LATITUDE').values
-y4 = train.pop('LONGITUDE').values
+y1 = sample_train.pop('BUILDINGID').values
+y2 = sample_train.pop('FLOOR').values
+y3 = sample_train.pop('LATITUDE').values
+y4 = sample_train.pop('LONGITUDE').values
 
-# Convert building and floor to categoric ()
-data['x'] = data['x'].astype('category')
+## Features and targets normalization (scaling)
 
-# Cross validation
+## Cross validation
 
-# Confusion matrix
-
-
-# Features and targets normalization
-scalarX, scalary = MinMaxScaler(), MinMaxScaler()
-scalarX.fit(X)
-scalary.fit(y)
+## Confusion matrix
 
 ## SVM
-#create an object of type LinearSVC
-svc_model = LinearSVC(random_state=0)
+# Create an object of type LinearSVC
+svc = LinearSVC(random_state=0, max_iter= 10000, verbose=2)  # verbose=2 to see the progress
 
-#train the algorithm on training data and predict using the testing data
-print (train.iloc[:, :520].shape)
-print (y1.shape)
-print (val.iloc[:, :520].shape)
-print (val.iloc[:, 523].shape)
-pred1 = svc_model.fit(train.iloc[:, 0:520], y1).predict(val.iloc[:, :520])
-
-#print the accuracy score of the model
-print ("LinearSVC accuracy : ", accuracy_score(val.iloc[:, 523], pred1, normalize = True))
-
-# Save model
-
+# Train on training data and predict using the testing data
+fit_svc1 = svc.fit(train_waps, y1)
+pred_svc1 = fit_svc1.predict(val_waps)
+svc.score(train_waps, y1)
+print ("LinearSVC accuracy : ", accuracy_score(val_waps, pred_svc1))
 
 
 ## KNN
-#create object of the lassifier
-neigh = KNeighborsClassifier(n_neighbors=3)
+# Create an object of the classifier
+knn1 = KNeighborsClassifier(n_neighbors=3)
 
-#Train the algorithm
-neigh.fit(train.iloc[:, :520], y1)
+# Train on training data and predict using the testing data
+fit_knn1 = knn1.fit(train_waps, y1)
+pred_knn1 = fit_knn1.predict(val_waps)
+knn1.score(train_waps, y1)
+accuracy_score(val_build, pred_knn1)
 
-# predict the response
-pred2 = neigh.predict(val.iloc[:, :520])
+## XGBoost
+# Create an object of the classifier
+xgb1 = XGBClassifier()
 
-# evaluate accuracy
-print ("KNeighbors accuracy score : ",accuracy_score(val.iloc[:, 523], pred2))
+# Train on training data and predict using the testing data
+fit_xgb1 = xgb1.fit(train_waps, y1)
+pred_xgb1 = fit_xgb1.predict(val_waps)
+xgb1.score(train_waps, y1)
+accuracy_score(val_build, pred_xgb1)
+
+# Save models to file
+# models = dict(fit_svc1 = fit_svc1, fit_knn1 = fit_knn1, fit_xgb1)
+# 
+# with open('data/wap_buildings.pkl', 'wb') as f:
+#     pickle.dump(wap_building, f)
+
